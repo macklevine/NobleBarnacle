1bab916c29fa57c70aec34606448d6efa0d081ea
jimtom2713 <jwilliamson528@gmail.com>
2015-08-10 14:38:44 -0700

    persitent model data between views

--- client/app/controllers/general.js
+++ client/app/controllers/general.js
@@ -1,6 +1,6 @@
-angular.module('mimo.general', [])
+/*angular.module('mimo.general', [])
   .controller('generalController', function ($scope, generalFactory) {
-  $scope.message = [];
+  $scope.model = generalFactory.model;
   $scope.getGeneral = function(){
     generalFactory.getGeneral().then(function(resp){
       $scope.message = resp.gAndA;
@@ -9,7 +9,9 @@ angular.module('mimo.general', [])
 })
   .factory('generalFactory', function ($http) {
     var factory = {};
+    factory.model = [1,2,3,4,5,6,7,8,9];
     factory.getGeneral = function(){
+      console.log('made http request');
       return $http({
         method:'GET',
         url:'/general'
@@ -18,4 +20,40 @@ angular.module('mimo.general', [])
       });
     }
     return factory;
+  })*/
+
+
+angular.module('mimo.general', [])
+  .controller('generalController', function ($scope, modelFactory){
+    $scope.showModel = function(){
+      if(!modelFactory.madeServerRequest){
+        modelFactory.getModel().then(function(resp){
+          $scope.general = modelFactory.model;
+        })
+      }else{
+        $scope.general = modelFactory.model;
+      }
+    }
+    $scope.update = function(){
+      modelFactory.getModel().then(function(response){
+        $scope.model = resp;
+      })
+    };
+  })
+  .factory('modelFactory', function ($http) {
+    var modelFactory = {};
+    modelFactory.madeServerRequest = false;
+    modelFactory.model = [];
+    modelFactory.getModel = function(){
+      console.log('made http request');
+      return $http({
+        method: 'GET',
+        url: '/general'
+      }).then(function(results){
+        modelFactory.madeServerRequest = true;
+        modelFactory.model = results.data;
+        // return results;
+      });
+    }
+    return modelFactory;
   })
