1aef138ef3d62b727ed3a95fca13da4a9dd7ddf9
jimtom2713 <jwilliamson528@gmail.com>
2015-08-11 15:14:24 -0700

    added d3 as a factory and angular directive for general display

--- client/app/controllers/mimoAngularConfig.js
+++ client/app/controllers/mimoAngularConfig.js
@@ -6,8 +6,7 @@ var mimo = angular.module('mimo', [
   'mimo.debtAndEquity',
   'mimo.general',
   'mimo.revenue',
-  'mimo.products',
-  'd3'
+  'mimo.products'
 ]);
 
 mimo.config(['$routeProvider', 
@@ -73,7 +72,8 @@ mimo.factory('d3Service', ['$document', '$window', '$q', '$rootScope',
   var scriptTag = $document[0].createElement('script');
   scriptTag.type = 'text/javascript'; 
   scriptTag.async = true;
-  scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+  // scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+  scriptTag.src = '../bower_components/d3/d3.min.js';
   scriptTag.onreadystatechange = function () {
     if (this.readyState == 'complete') onScriptLoad();
   }
@@ -86,101 +86,105 @@ mimo.factory('d3Service', ['$document', '$window', '$q', '$rootScope',
 }]);
 
 // angular.module('d3AngularApp', ['d3'])
-mimo.directive('d3Bars', ['$window', '$timeout', 'd3Service', 
-  function($window, $timeout, d3Service) {
-    return {
-      restrict: 'A',
-      scope: {
-        data: '=',
-        label: '@',
-        onClick: '&'
-      },
-      link: function(scope, ele, attrs) {
-        d3Service.d3().then(function(d3) {
+// mimo.directive('d3Bars', ['$window', '$timeout', 'd3Service', 
+//   function($window, $timeout, d3Service) {
+//     return {
+//       restrict: 'A',
+//       scope: {
+//         data: '=',
+//         label: '@',
+//         onClick: '&'
+//       },
+//       link: function(scope, ele, attrs) {
+//         d3Service.d3().then(function(d3) {
 
-          var renderTimeout;
-          var margin = parseInt(attrs.margin) || 20,
-              barHeight = parseInt(attrs.barHeight) || 20,
-              barPadding = parseInt(attrs.barPadding) || 5;
+//           var renderTimeout;
+//           var margin = parseInt(attrs.margin) || 20,
+//               barHeight = parseInt(attrs.barHeight) || 20,
+//               barPadding = parseInt(attrs.barPadding) || 5;
 
-          var svg = d3.select(ele[0])
-            .append('svg')
-            .style('width', '100%');
+//           var svg = d3.select(ele[0])
+//             .append('svg')
+//             .style('width', '40em');
+//             // .style('height', '100%');
 
-          $window.onresize = function() {
-            scope.$apply();
-          };
+//           $window.onresize = function() {
+//             scope.$apply();
+//           };
 
-          scope.data = [
-                 {name: "Greg", score: 98},
-                 {name: "Ari", score: 96},
-                 {name: 'Q', score: 75},
-                 {name: "Loser", score: 48}
-               ];
+//           scope.data = [
+//                  {name: "Greg", score: 98},
+//                  {name: "Ari", score: 96},
+//                  {name: 'Q', score: 75},
+//                  {name: "Loser", score: 48},
+//                  {name: "Loser", score: 48},
+//                  {name: "Loser", score: 48},
+//                  {name: "Loser", score: 48}
+//                ];
 
-          scope.$watch(function() {
-            return angular.element($window)[0].innerWidth;
-          }, function() {
-            scope.render(scope.data);
-          });
+//           scope.$watch(function() {
+//             return angular.element($window)[0].innerWidth;
+//           }, function() {
+//             scope.render(scope.data);
+//           });
 
-          scope.$watch('data', function(newData) {
-            scope.render(newData);
-          }, true);
+//           scope.$watch('data', function(newData) {
+//             scope.render(newData);
+//           }, true);
 
-          scope.render = function(data) {
-            svg.selectAll('*').remove();
+//           scope.render = function(data) {
+//             svg.selectAll('*').remove();
 
-            if (!data) return;
-            if (renderTimeout) clearTimeout(renderTimeout);
+//             if (!data) return;
+//             if (renderTimeout) clearTimeout(renderTimeout);
 
-            renderTimeout = $timeout(function() {
-              var width = d3.select(ele[0])[0][0].offsetWidth - margin,
-                  height = scope.data.length * (barHeight + barPadding),
-                  color = d3.scale.category20(),
-                  xScale = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) {
-                      return d.score;
-                    })])
-                    .range([0, width]);
+//             renderTimeout = $timeout(function() {
+//               var width = d3.select(ele[0])[0][0].offsetWidth - margin,
+//                   height = scope.data.length * (barHeight + barPadding),
+//                   color = d3.scale.category20(),
+//                   xScale = d3.scale.linear()
+//                     .domain([0, d3.max(data, function(d) {
+//                       return d.score;
+//                     })])
+//                     .range([0, width]);
 
-              svg.attr('height', height);
+//               svg.attr('height', height);
 
-              svg.selectAll('rect')
-                .data(data)
-                .enter()
-                  .append('rect')
-                  .on('click', function(d,i) {
-                    return scope.onClick({item: d});
-                  })
-                  .attr('height', barHeight)
-                  .attr('width', 140)
-                  .attr('x', Math.round(margin/2))
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding);
-                  })
-                  .attr('fill', function(d) {
-                    return color(d.score);
-                  })
-                  .transition()
-                    .duration(1000)
-                    .attr('width', function(d) {
-                      return xScale(d.score);
-                    });
-              svg.selectAll('text')
-                .data(data)
-                .enter()
-                  .append('text')
-                  .attr('fill', '#fff')
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding) + 15;
-                  })
-                  .attr('x', 15)
-                  .text(function(d) {
-                    return d.name + " (scored: " + d.score + ")";
-                  });
-            }, 200);
-          };
-        });
-      }}
-}])
+//               svg.selectAll('rect')
+//                 .data(data)
+//                 .enter()
+//                   .append('rect')
+//                   .on('click', function(d,i) {
+//                     return scope.onClick({item: d});
+//                   })
+//                   .attr('height', barHeight)
+//                   .attr('width', 140)
+//                   .attr('x', Math.round(margin/2))
+//                   .attr('y', function(d,i) {
+//                     return i * (barHeight + barPadding);
+//                   })
+//                   .attr('fill', function(d) {
+//                     return color(d.score);
+//                   })
+//                   .transition()
+//                     .duration(1000)
+//                     .attr('width', function(d) {
+//                       return xScale(d.score);
+//                     });
+//               svg.selectAll('text')
+//                 .data(data)
+//                 .enter()
+//                   .append('text')
+//                   .attr('fill', '#fff')
+//                   .attr('y', function(d,i) {
+//                     return i * (barHeight + barPadding) + 15;
+//                   })
+//                   .attr('x', 15)
+//                   .text(function(d) {
+//                     return d.name + " (scored: " + d.score + ")";
+//                   });
+//             }, 200);
+//           };
+//         });
+//       }}
+// }])
