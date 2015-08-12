8305ff766e88936289dec12cd8e7bc770e60a833
jimtom2713 <jwilliamson528@gmail.com>
2015-08-12 12:24:55 -0700

    comments to angular controllers and d3 directive tempaltes

--- client/app/controllers/model.js
+++ client/app/controllers/model.js
@@ -1,11 +1,39 @@
 angular.module('mimo.model', [])
   .controller('modelController', function ($scope, modelFactory){
-  // $scope.model = 'Model Controller';
   $scope.model = modelFactory.model;
   $scope.getModel = function(){
     modelFactory.getModel().then(function(results){
       console.log('Recieved Model for user');
-      // $scope.model = results.model;
     })
   }
 })
+//Uncomment below code for data manipulation factory
+/*
+  .factory('modelDataFactory', function(){
+    modelDataFactory = {};
+      //place code for manipulation the model data here
+
+    return modelDataFactory;
+})
+*/
+//Uncomment below code for custom D3 directive
+/*
+  .directive('d3Employee', ['$window', '$timeout', 'd3Service', 
+    function($window, $timeout, d3Service) {
+      return {
+        restrict: 'ACE',
+        //NOTE TO MYSELF: Replace this with the proper scoping to the data object..
+        // something like this... scope:{data:'='}
+        scope: true,
+        //run as a link directive rather that the compile directive
+        //this runs once the html/directives are compiiled and they will
+        //be ready to go when the view is loaded.
+        link: function(scope, ele, attrs) {
+          d3Service.d3().then(function(d3) {
+            //place your D3 code here
+            //IMPORTANT...
+            //remember your code needs to operate on the scope.data object
+          });
+        }}
+    }])
+*/
--- client/app/controllers/products.js
+++ client/app/controllers/products.js
@@ -3,3 +3,33 @@ angular.module('mimo.products', [])
   // $scope.products = modelFactory.model.products;
   $scope.products = "Products Controller";
 });
+//Uncomment below code for data manipulation factory
+/*
+  .factory('productsFact', function(){
+    productsFact = {};
+      //place code for manipulation the products data here
+
+    return productsFact;
+})
+*/
+//Uncomment below code for custom D3 directive
+/*
+  .directive('d3Employee', ['$window', '$timeout', 'd3Service', 
+    function($window, $timeout, d3Service) {
+      return {
+        restrict: 'ACE',
+        //NOTE TO MYSELF: Replace this with the proper scoping to the data object..
+        // something like this... scope:{data:'='}
+        scope: true,
+        //run as a link directive rather that the compile directive
+        //this runs once the html/directives are compiiled and they will
+        //be ready to go when the view is loaded.
+        link: function(scope, ele, attrs) {
+          d3Service.d3().then(function(d3) {
+            //place your D3 code here
+            //IMPORTANT...
+            //remember your code needs to operate on the scope.data object
+          });
+        }}
+    }])
+*/
