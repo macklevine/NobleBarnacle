ced5d25395ae5d8d2a0049c507c3b2b166f8faea
jimtom2713 <jwilliamson528@gmail.com>
2015-08-13 15:02:32 -0700

    cleaned up and commented start up costs

--- client/app/controllers/startup.js
+++ client/app/controllers/startup.js
@@ -1,25 +1,32 @@
-angular.module('mimo.startup', [])
-  .controller('startupController', function ($scope, dataFactory){
-  $scope.startupCosts = dataFactory.model.expenses.startupCosts;
-})
-  //Uncomment below code for data manipulation factory
 /*
+Angular module for start up costs within the financial model
+
+$scope.data -> contains the data related to start up costs for the
+                        financial model. Accessed from the dataFactory object
+                        without an extra HTTP request.
+
+RECALL: Within dataFactory.model you have access to the entire model here if you need
+        any of that information for analytics or visulaizations. We've only pulled in
+        the specific information for display. 
+*/
+
+angular.module('mimo.startup', [])
+  .controller('startupController', function ($scope, dataFactory, startupFactory){
+  $scope.data = dataFactory.model.expenses.startupCosts;
+})
   .factory('startupFactory', function(){
     startupFactory = {};
-      //place code for manipulation the startup data here
+      //place code for manipulation the products data here
 
     return startupFactory;
 })
-*/
-//Uncomment below code for custom D3 directive
-/*
-  .directive('d3Employee', ['$window', '$timeout', 'd3Service', 
+  .directive('d3Startup', ['$window', '$timeout', 'd3Service', 
     function($window, $timeout, d3Service) {
       return {
         restrict: 'ACE',
-        //NOTE TO MYSELF: Replace this with the proper scoping to the data object..
-        // something like this... scope:{data:'='}
-        scope: true,
+        scope: {
+          data: "=" //this binds $scope.data to scope.data. Use scope.data inside D3
+        },
         //run as a link directive rather that the compile directive
         //this runs once the html/directives are compiiled and they will
         //be ready to go when the view is loaded.
@@ -31,4 +38,3 @@ angular.module('mimo.startup', [])
           });
         }}
     }])
-*/
